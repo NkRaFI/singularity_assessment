f8d2244f38f7566adeb895e280d4634d57cacfd9
NkRaFI <nkrafi11@gmail.com>
2023-12-25 16:33:44 +0600

    backend api done

---
+++ frontend/src/features/music/musicSlice.js
@@ -0,0 +1,17 @@
+import { apiSlice } from "../api/apiSlice";
+
+
+export const musicSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        searchMusic: builder.mutation({
+            query: ({ query }) => ({
+                url: `/music/search?${query ? `query=${query}` : ""}`,
+                method: "GET"
+            })
+        })
+    })
+})
+
+export const {
+    useSearchMusicMutation
+} = musicSlice
